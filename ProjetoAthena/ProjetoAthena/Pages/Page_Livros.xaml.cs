056d9a9c56c1adc0c0f50711b2e8a029aa803c94
Matheus Solha <mcsolha@gmail.com>
2015-10-05 11:52:48 -0300

    Projeto migrado para versão mais recente da IDE

---
+++ ProjetoAthena/ProjetoAthena/Pages/Page_Livros.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ProjetoAthena.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Page_Livros : Page
+    {
+        public Page_Livros()
+        {
+            this.InitializeComponent();
+        }
+        private void voltar_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage));
+        }
+    }
+}
