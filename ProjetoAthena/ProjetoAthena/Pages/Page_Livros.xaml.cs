b83b9c8d02bb0b5af33fb9ca0fe1e4dc8cc0ff63
Matheus Solha <mcsolha@gmail.com>
2015-11-12 19:42:10 -0200

    pop-ups erros

--- ProjetoAthena/ProjetoAthena/Pages/Page_Livros.xaml.cs
+++ ProjetoAthena/ProjetoAthena/Pages/Page_Livros.xaml.cs
@@ -6,6 +6,7 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Core;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -17,12 +18,15 @@ using Windows.UI.Xaml.Navigation;
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace ProjetoAthena.Pages
-{
+{    
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class Page_Livros : Page
-    {        
+    {
+        private const int erroConexao = 0,
+                            erroAcessoSite = 1,
+                            erroSemLivros = 2;
         public Page_Livros()
         {
             this.InitializeComponent();
@@ -36,52 +40,74 @@ namespace ProjetoAthena.Pages
             this.Frame.Navigate(typeof(MainPage));
         }
 
+        /// <summary>
+        /// Show pop-up about an error.
+        /// </summary>
+        private async void Erro(int error)
+        {
+            switch (error)
+            {
+                case 0:
+                    var connection = new MessageDialog("Sem Conexão!");
+                    await connection.ShowAsync();
+                    break;
+                case 1:
+                    var unkownError = new MessageDialog("Erro! Tente novamente.");
+                    await unkownError.ShowAsync();
+                    break;
+                case 2:
+                    var books = new MessageDialog("Sem livros para renovar");
+                    await books.ShowAsync();
+                    break;
+                default:
+                    break;
+            }            
+        }
+
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            base.OnNavigatedTo(e);
-            App.DataConexao.RetornarLivros((IAsyncResult resultado)=>
+            base.OnNavigatedTo(e);            
+            if (!App.NetWorkAvailable)
             {
-                bool reservado = false;
-                foreach (ItemViewModel item in App.ViewModel.Items)
+                Erro(erroConexao);
+            }
+            else
+            {
+                App.DataConexao.RetornarLivros((IAsyncResult resultado) =>
                 {
-                    if (item.Reservado)
+                    int number = 0;
+                    foreach (ItemViewModel item in App.ViewModel.Items)
                     {
-                        reservado = true;
-                        break;
+                        int totalDias = (item.DataDevolucao - DateTime.Now).Days;
+                        switch (number)
+                        {
+                            case 0:
+                                livro1.Text = item.Titulo;
+                                datadev1.Text = item.StringDevolucao;
+                                temporest1.Text = "Devolver em " + totalDias + " dias";
+                                break;
+                            case 1:
+                                livro2.Text = item.Titulo;
+                                datadev2.Text = item.StringDevolucao;
+                                temporest2.Text = "Devolver em " + totalDias + " dias";
+                                break;
+                            case 2:
+                                livro3.Text = item.Titulo;
+                                datadev3.Text = item.StringDevolucao;
+                                temporest3.Text = "Devolver em " + totalDias + " dias";
+                                break;
+                            case 3:
+                                livro4.Text = item.Titulo;
+                                datadev4.Text = item.StringDevolucao;
+                                temporest4.Text = "Devolver em " + totalDias + " dias";
+                                break;
+                            default:
+                                break;
+                        }
+                        number++;
                     }
-                }
-                int number = 0;                
-                foreach (ItemViewModel item in App.ViewModel.Items)
-                {
-                    int totalDias = (item.DataDevolucao - DateTime.Now).Days;
-                    switch (number)
-                    {
-                        case 0:
-                            livro1.Text = item.Titulo;
-                            datadev1.Text = item.StringDevolucao;
-                            temporest1.Text = "Devolver em " + totalDias + " dias";
-                            break;
-                        case 1:
-                            livro2.Text = item.Titulo;
-                            datadev2.Text = item.StringDevolucao;
-                            temporest2.Text = "Devolver em " + totalDias + " dias";
-                            break;
-                        case 2:
-                            livro3.Text = item.Titulo;
-                            datadev3.Text = item.StringDevolucao;
-                            temporest3.Text = "Devolver em " + totalDias + " dias";
-                            break;
-                        case 3:
-                            livro4.Text = item.Titulo;
-                            datadev4.Text = item.StringDevolucao;
-                            temporest4.Text = "Devolver em " + totalDias + " dias";
-                            break;
-                        default:
-                            break;
-                    }
-                    number++;
-                }
-            });
+                });
+            }            
         }
 
 
@@ -116,28 +142,18 @@ namespace ProjetoAthena.Pages
                     App.DataConexao.RenovarLivros(RenLivrosCallback);
                 }
                 else
-                {
-                    livro1.Text = "sem conexão";
-
+                {                    
+                    Erro(erroConexao);
                 }
             }
             else
             {
-                livro1.Text = "sem livros pra renovar";
+                Erro(erroSemLivros);
             }
         }
 
         void MostraLivros()
-        {
-            bool reservado = false;
-            foreach (ItemViewModel item in App.ViewModel.Items)
-            {
-                if (item.Reservado)
-                {
-                    reservado = true;
-                    break;
-                }
-            }
+        {            
             int number = 0;
             foreach (ItemViewModel item in App.ViewModel.Items)
             {
@@ -176,7 +192,7 @@ namespace ProjetoAthena.Pages
             {
                 var ignored = Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal,() => 
                 {
-                    livro1.Text = "erro";
+                    Erro(erroAcessoSite);
                 });
             }
             else
